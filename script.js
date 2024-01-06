352ec6614475b912b62d0d7f7107e615e8517efc
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2024-01-06 21:42:44 +0000

    Complete basic calculator features

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function divide(x, y) {
   return x / y;
 }
 
-let x;
-let y;
-let operator;
+let x = '';
+let y = '';
+let operator = '';
 
 function operate(operator, x, y) {
   switch (operator) {
@@ -27,8 +27,50 @@ function operate(operator, x, y) {
     case '*':
       return multiply(x, y);
     case '/':
+      if (!y) {
+        return 'Undefined';
+      }
       return divide(x, y);
     default:
-      return 'Invalid operator';
+      return 'Invalid Operator';
   }
 }
+
+function display(number) {
+  document.querySelector('#display').textContent = number;
+}
+
+const digits = document.querySelectorAll('.digits');
+digits.forEach(button => {
+  button.addEventListener('click', () => {
+    if (operator === '=') {
+      x = '';
+    }
+    y = y.replace(/^0/, '');
+    y += button.textContent;
+    display(y);
+  });
+});
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach(button => {
+  button.addEventListener('click', () => {
+    if (y) {
+      if (x && operator) {
+        x = operate(operator, parseFloat(x), parseFloat(y));
+        display(Math.round(x * 100) / 100);
+      } else {
+        x = y;
+      }
+      y = '';
+    }
+    operator = button.textContent;
+  });
+});
+
+document.querySelector('#clear').addEventListener('click', () => {
+  x = '';
+  y = '';
+  operator = '';
+  display('0');
+});
